d0c70a4d8de1b1f78e9e458eb89aaf518512d2a5
Nick Tremaroli <nicktrem@vt.edu>
2022-10-18 11:42:02 -0400

    Cleaned up and documented the ADC more

--- Tiva-Consolidated/HAL/ADC_TIVA.c
+++ Tiva-Consolidated/HAL/ADC_TIVA.c
@@ -1,33 +1,70 @@
+/**
+ * ADC_TIVA.c
+ * @author: Nick Tremaroli
+ * Contains all of the low-level code for ADC related functions
+ */
 #include "ADC_TIVA.h"
 
+/**
+ * ADCConfig0
+ * Enables ADC0 to be working on Pin E5 on the Tiva
+ */
 void ADCConfig0()
 {
+    // Enable ADC 0
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);            //Port E setup for ADC conversion.
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0)){}     //Wait for the ADC module to be ready.
 
-//For Software Averaging.  Can add in hardware averaging later if needed.
-    ADCSequenceDisable(ADC0_BASE, 0);                    //Disable ADC0 before configuring.
-    ADCSoftwareOversampleConfigure(ADC0_BASE, 0, 8);     //Enable software over-sampling for software averaging of ADC values.
-    ADCSoftwareOversampleStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH8); //Originally channel 8
-    ADCSequenceEnable(ADC0_BASE, 0);                     //Enable the ADC sequence.
+    // Enable GPIO Port E because ADC0 uses pin E5
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
 
-    ADCIntEnableEx(ADC0_BASE, ADC_INT_SS0);              //Enable interrupt on ADC0 sequencer 1.
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_5); // Originally pin 5
+    // wait for the ADC module to be ready
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0));
+
+    // Disable the ADC before configuring
+    ADCSequenceDisable(ADC0_BASE, 0);
+
+    // Enable software over-sampling for software averaging of ADC values.
+    ADCSoftwareOversampleConfigure(ADC0_BASE, 0, 8);
+    ADCSoftwareOversampleStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH8);
+
+    // Enable the ADC sequence
+    ADCSequenceEnable(ADC0_BASE, 0);
+
+    // Enable interrupt on ADC0 sequencer 1
+    ADCIntEnableEx(ADC0_BASE, ADC_INT_SS0);
+
+    // Enable GPIO Pin E5 for the ADC
+    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_5);
 }
 
+/**
+ * ADCConfig1
+ * Enables ADC1 to be working on Pin E2 on the Tiva
+ */
 void ADCConfig1()
 {
+    // Enable ADC 1
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC1);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);            //Port E setup for ADC conversion.
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC1)){}     //Wait for the ADC module to be ready.
 
-//For Software Averaging.  Can add in hardware averaging later if needed.
-    ADCSequenceDisable(ADC1_BASE, 0);                    //Disable ADC1 before configuring.
+    // Enable GPIO Port E because ADC1 uses pin E2
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+
+    // Wait for the ADC module to be ready
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC1));
+
+    // Disable ADC1 before configuring
+    ADCSequenceDisable(ADC1_BASE, 0);
+
+    // Enable software over-sampling for software averaging of ADC values.
     ADCSoftwareOversampleConfigure(ADC1_BASE, 0, 8);
     ADCSoftwareOversampleStepConfigure(ADC1_BASE, 0, 0, ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH1); // Originally channel 1
-    ADCSequenceEnable(ADC1_BASE, 0);                     //Enable the ADC sequence.
 
+    // Enable the ADC sequence
+    ADCSequenceEnable(ADC1_BASE, 0);
+
+    // Enable interrupt on ADC1 sequencer 1
     ADCIntEnableEx(ADC1_BASE, ADC_INT_SS0);
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2); // Originally pin 2
+
+    // Enable GPIO Pin E2 for the ADC
+    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_2);
 }
