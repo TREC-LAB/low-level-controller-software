15cb8685cd5b4bfad54e6e57cb6be672314cd2f3
Nick Tremaroli <nicktrem@vt.edu>
2022-10-18 11:42:27 -0400

    Added more documentation to CAN

--- Tiva-Consolidated/HAL/CAN_TI_TIVA.c
+++ Tiva-Consolidated/HAL/CAN_TI_TIVA.c
@@ -1,87 +1,139 @@
-/*
- * myCAN.c
- *
- *  Created on: May 11, 2021
- *      Author: Chi
+/**
+ * CAN_TI_TIVA.c
+ * @author: Nick Tremaroli
+ * Contains all of the low-level code for CAN communication
+ * on the TIVA
  */
-
 
 #include "CAN_TI_TIVA.h"
 
-
-void ReadCAN(CANBUSRx* msgRx0, CANBUSRx* msgRx1)
+/**
+ * ReadCAN
+ * read the can data from the CAN Bus given the Rx
+ * @param canMsgRx: a pointer to the CANBUSRx data structure
+ * to which the CAN data to be read will be stored
+ */
+void ReadCAN(CANBUSRx* canMsgRx)
 {
-    uint32_t status = CANStatusGet(CAN0_BASE, CAN_STS_CONTROL);
-
-    CANMessageGet(CAN0_BASE, 2, &msgRx0->msgRx, 1);
-    CANMessageGet(CAN0_BASE, 3, &msgRx1->msgRx, 1);
+    // data gets stored in canMgRx->msgRx
+    CANMessageGet(CAN0_BASE, canMsgRx->canObjId, &canMsgRx->msgRx, 1);
 }
 
-float binTwoCToDec(unsigned char com0, unsigned char com1)
+/**
+ * BinaryTwoDecimal
+ * Converts two unsigned bytes to a decimal number.
+ * This decimal number is then casted to a float
+ * @param com0: the Least Significant Byte (LSB) of data to convert
+ * @param com1: the Most Significant Byte (MSB) of data to convert
+ * @return: the decimal value after conversion casted to a float
+ */
+float BinaryToDecimal(unsigned char com0, unsigned char com1)
 {
-    //this is the real money
-    int output;
+    int output = 0;
+    // combine the LSB and the MSB
     int conct = (com1 << 8 | com0);
+
+    // account for the negative case
     const int negative = ( conct & (1 << 15)) != 0;
     if (negative)
         output = conct | ~((1 << 15) - 1);
     else
         output = conct;
 
+    // return by casting the output to a float
     return (float) output;
 }
 
-
+/**
+ * CANInitial
+ * Initialize CAN Base 0 for communication using
+ * Pins B4 and B5
+ * @param canRate: The rate at which CAN will operate
+ */
 void CANInitial(uint32_t canRate)
 {
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE); // enable GPIOE peripheral for CAN0
-    GPIOPinConfigure(GPIO_PE4_CAN0RX);
-    GPIOPinConfigure(GPIO_PE5_CAN0TX);
-    GPIOPinTypeCAN(GPIO_PORTE_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    GPIOPinConfigure(GPIO_PB4_CAN0RX);
+    GPIOPinConfigure(GPIO_PB5_CAN0TX);
+    GPIOPinTypeCAN(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_CAN0)){
-    }
     CANInit(CAN0_BASE);
     CANBitRateSet(CAN0_BASE, SysCtlClockGet(), canRate); // set CAN communication rate to 500kbps, same as ATI CAN rate
-//    CANIntRegister(CAN0_BASE, CANIntHandler); // use dynamic vector table allocation
-//    CANIntEnable(CAN0_BASE, CAN_INT_MASTER | CAN_INT_ERROR | CAN_INT_STATUS);
-//    IntEnable(INT_CAN0);
     CANEnable(CAN0_BASE);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_CAN0));
 }
 
-void CANRx0Initial(CANBUSRx* msgRx0, uint16_t canID, uint16_t canObj){
-    // Set up msg object 2
-    msgRx0->msgRx.ui32MsgID = 0x1b5; //ID of the board
-    msgRx0->msgRx.ui32MsgIDMask = 0x1b5;
-    msgRx0->msgRx.ui32Flags = MSG_OBJ_RX_INT_ENABLE | MSG_OBJ_USE_ID_FILTER; //flag this object as Rx and use Mask
+/**
+ * CANRx0Initial
+ * Initializes the receive object number 0 for the CAN bus
+ * @param msgRx0: a pointer to the CANBUSRx object which stores all
+ * of the receive settings
+ * @param canID: The CAN message identifier
+ * @param canObj: The CAN object ID
+ */
+void CANRx0Initial(CANBUSRx* msgRx0, uint16_t canID, uint16_t canObj)
+{
+    // setup this CAN Object
+    msgRx0->canObjId = canObj;
+
+    // the ID of the board
+    msgRx0->msgRx.ui32MsgID = canID;
+    msgRx0->msgRx.ui32MsgIDMask = canID;
+
+    //flag this object as Rx and use Mask
+    msgRx0->msgRx.ui32Flags = MSG_OBJ_RX_INT_ENABLE | MSG_OBJ_USE_ID_FILTER;
+
     msgRx0->msgRx.ui32MsgLen = 8;
-    CANMessageSet(CAN0_BASE, canObj, &msgRx0->msgRx, MSG_OBJ_TYPE_RX);
+    CANMessageSet(CAN0_BASE, msgRx0->canObjId, &msgRx0->msgRx, MSG_OBJ_TYPE_RX);
     msgRx0->msgRx.pui8MsgData = msgRx0->msgRxData;
-
 }
 
+/**
+ * CANRx1Initial
+ * Initializes the receive object number 1 for the CAN bus
+ * @param msgRx1: a pointer to the CANBUSRx object which stores all
+ * of the receive settings
+ * @param canID: The CAN message identifier
+ * @param canObj: The CAN object ID
+ */
 void CANRx1Initial(CANBUSRx* msgRx1, uint16_t canID, uint16_t canObj)
 {
-    // Set up msg object 3
-    msgRx1->msgRx.ui32MsgID = 0x1b6; //ID of the board
-    msgRx1->msgRx.ui32MsgIDMask = 0x1b6;
-    msgRx1->msgRx.ui32Flags = MSG_OBJ_RX_INT_ENABLE | MSG_OBJ_USE_ID_FILTER; //flag this object as Rx and use Mask
+    // setup this CAN Object
+    msgRx1->canObjId = canObj;
+
+    // the ID of the board
+    msgRx1->msgRx.ui32MsgID = canID;
+    msgRx1->msgRx.ui32MsgIDMask = canID;
+
+    //flag this object as Rx and use Mask
+    msgRx1->msgRx.ui32Flags = MSG_OBJ_RX_INT_ENABLE | MSG_OBJ_USE_ID_FILTER;
+
     msgRx1->msgRx.ui32MsgLen = 8;
     CANMessageSet(CAN0_BASE, canObj, &msgRx1->msgRx, MSG_OBJ_TYPE_RX);
     msgRx1->msgRx.pui8MsgData = msgRx1->msgRxData;
 }
 
-void CANRxInitial(uint32_t msgId, uint32_t msgIdMas)
+/**
+ * CANTxInitial
+ * Initializes the transer object for the CAN bus
+ * @param msgTx0: a pointer to the CANBUSTx object which stores all
+ * of the receive settings
+ * @param canID: The CAN message identifier
+ * @param canObj: The CAN object ID
+ */
+void CANTxInitial(CANBUSTx* msgTx0, uint16_t canID, uint16_t canObj)
 {
+    // setup this CAN Object
+    msgTx0->canObjId = canObj;
 
-}
-
-void CANTxInitial(CANBUSTx* msgTx0, uint16_t canID, uint16_t canObj){
-    // Set up msg object
-//    msgTxData = 0;
+    // setup the data pointer
     msgTx0->msgTxDataPtr = (unsigned char *)msgTx0->msgTxData;
+
+    // the ID of the board
     msgTx0->msgTx.ui32MsgID = canID;
     msgTx0->msgTx.ui32MsgIDMask = canObj;
+
+
     msgTx0->msgTx.ui32Flags = MSG_OBJ_TX_INT_ENABLE;
     msgTx0->msgTx.ui32MsgLen = sizeof(msgTx0->msgTxDataPtr);
     msgTx0->msgTx.pui8MsgData = msgTx0->msgTxDataPtr;
@@ -91,7 +143,12 @@ void CANTxInitial(CANBUSTx* msgTx0, uint16_t canID, uint16_t canObj){
     msgTx0->msgTxDataPtr[3] = 0x00;
 }
 
-void CANSend(CANBUSTx* msgTx0, uint16_t canID, uint16_t canObj)
+/**
+ * CANSend
+ * Sends CAN data to the tx object
+ * @param msgTx0: a pointer to the Tx object to send data too
+ */
+void CANSend(CANBUSTx* msgTx0)
 {
-    CANMessageSet(CAN0_BASE, canObj, &msgTx0->msgTx, MSG_OBJ_TYPE_TX);
+    CANMessageSet(CAN0_BASE, msgTx0->canObjId, &msgTx0->msgTx, MSG_OBJ_TYPE_TX);
 }
