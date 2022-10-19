96ec597ad0ac712e098220cb269435cc0796c92f
Nick Tremaroli <nicktrem@vt.edu>
2022-10-19 11:22:56 -0400

    Added PWM generator struct

--- Tiva-Consolidated/HAL/PWM_Driver.h
+++ Tiva-Consolidated/HAL/PWM_Driver.h
@@ -1,10 +1,9 @@
-/*
-*This is the header file for sendind the PWM signals to the BLDC motors for motor control.
-*
-*Code Created by
-*   Brandyn Greczek, Date: February 2, 2018.
-*/
-
+/**
+ * PWM_Driver.h
+ * @author: Nick Tremaroli
+ * Contains the layout and functions regarding the Tiva's
+ * generation of a PWM
+ */
 
 #ifndef PWM_DRIVER_H
 #define PWM_DRIVER_H
@@ -20,10 +19,32 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/pwm.h"
 
-//Function Prototypes
+/**
+ * PWMGenerator
+ * a struct which contains all of the data
+ * needed to generate a PWM signal
+ */
+struct PWMGenerator
+{
+    float dutyCycle;
+    uint16_t pwmFrequency;
+    uint8_t direction;
+};
+typedef struct PWMGenerator PWMGenerator;
+
+// Configure the PWM
 void PWMConfig(void);
-void setPulseWidth(uint8_t actuator, uint16_t pwmFrequency, float dc, uint32_t SysClock, uint8_t dir);            //Provide a desired integer PWM Frequency, followed by an integer duty cycle.
-uint8_t getDutyCycle(void);                                             //Get the current duty cycle of the PWM signal.
-uint32_t getPWMFrequency(void);                                         //Get the current frequency of the PWM signal.
+
+// Construct a PWM generator object
+PWMGenerator PWMGeneratorConstruct(uint16_t pwmFrequency);
+
+//Provide a desired integer PWM Frequency, followed by an integer duty cycle.
+void setPulseWidth(uint8_t actuator, PWMGenerator* pwmGenerator, uint32_t sysClock);
+
+//Get the current duty cycle of the PWM signal.
+uint8_t getDutyCycle(void);
+
+//Get the current frequency of the PWM signal.
+uint32_t getPWMFrequency(void);
 
 #endif	/* PWM_DRIVER_H_ */
