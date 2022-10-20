b4ce7027679eb1b6c041117fe8c2c3196fd4763c
Nick Tremaroli <nicktrem@vt.edu>
2022-10-20 10:32:46 -0400

    Cleaned up the timer code

--- Tiva-Consolidated/HAL/Timer_TIVA.h
+++ Tiva-Consolidated/HAL/Timer_TIVA.h
@@ -1,8 +1,8 @@
-/*
- * myTimer.h
- *
- *  Created on: Aug 27, 2018
- *      Author: Zhoubao Pang
+/**
+ * Timer_TIVA.h
+ * @author: Nick Tremaroli
+ * Contains the layout and functions regarding timer
+ * related functions of the Tiva
  */
 
 #ifndef TIMER_TIVA_H_
@@ -19,12 +19,22 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/timer.h"
 
+// Configure timer 1
 void timer1A_Config();
+
+// Configure timer 2
 void timer2A_Config();
+
+// Configure timer 3
 void timer3A_Config();
 
+// starts timer 1
 void startTimer1(uint16_t rate);
+
+// starts timer 2
 void startTimer2(uint16_t rate);
+
+// starts timer 3
 void startTimer3(uint16_t rate);
 
 #endif /* TIMER_TIVA_H_ */
